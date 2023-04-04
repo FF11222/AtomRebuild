9c9b3a737bdc3be075308ec95e9bab5488ea984e
Patrick <tingyuchang960625@gmail.com>
2023-04-04 17:58:57 +0800

    Initial commit

---
+++ src/main/java/com/patrickchang/atomrebuild/AtomRebuild.java
@@ -0,0 +1,8 @@
+package com.patrickchang.atomrebuild;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(AtomRebuild.MOD_ID)
+public class AtomRebuild {
+    public static final String MOD_ID = "atomrebuild";
+}
